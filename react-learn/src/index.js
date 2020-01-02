26f23c3a6a4203a4a3c92475da9807c1f39af7be
zt_zengc <zt_zengc@agree.com.cn>
2020-01-02 17:24:05 +0800

    修改修改

--- react-learn/src/index.js
+++ react-learn/src/index.js
@@ -1,4 +1,4 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<h1>Hello World <span>span元素</span></h1>, document.getElementById('root'));
+ReactDOM.render(<h1></h1>, document.getElementById('root'));
