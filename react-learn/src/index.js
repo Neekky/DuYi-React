ad101cf89debae0bd95134ceea742babb2ed0d04
zt_zengc <zt_zengc@agree.com.cn>
2020-01-03 11:39:39 +0800

    修改

--- react-learn/src/index.js
+++ react-learn/src/index.js
@@ -7,7 +7,7 @@ setInterval(() => {
     num++;
     const div = (
         <div title="asdfadf">
-            {num}
+            
         </div>
     );
     ReactDOM.render(div, document.getElementById("root"));
