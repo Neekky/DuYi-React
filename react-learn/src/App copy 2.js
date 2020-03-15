f148c78e6291b9bd93ef1d1039b954dff818447d
cengchao <475383200@qq.com>
2020-03-15 21:33:20 +0800

    add

--- react-learn/src/App copy 2.js
+++ react-learn/src/App copy 2.js
@@ -1,6 +1,8 @@
-import React, { useState, useReducer } from 'react'
+import React, { useState, useReducer, useEffect, useCallback } from 'react'
 
 window.arr = [];
+
+const set = new Set();
 
 function init(initialCount) {
     return { count: initialCount };
@@ -21,21 +23,57 @@ function reducer(state, action) {
     }
 }
 
-export default function App() {
-    const [state, dispatch] = useReducer(reducer, 1, init);
 
-    return (
-        <>
-            count: {state.count}
-            <button
-                onClick={() => dispatch({ type: 'reset', payload: 5 })}>
-                Reset
-            </button>
-            <button onClick={() => dispatch({ type: 'decrement' })}>-</button>
-            <button onClick={() => dispatch({ type: 'increment' })}>+</button>
-        </>
-    )
+
+export default function Parent() {
+    const [count, setCount] = useState(1);
+    const [val, setVal] = useState('');
+ 
+    const callback = useCallback(() => {
+        return count;
+    }, [count]);
+    return <div>
+        <h4>父组件{count}</h4>
+        <Child callback={callback}/>
+        <div>
+            <button onClick={() => setCount(count + 1)}>+</button>
+            <input value={val} onChange={event => setVal(event.target.value)}/>
+        </div>
+    </div>;
 }
+
+function Child({ callback }) {
+    const [count, setCount] = useState(callback());
+    useEffect(() => {
+        setCount(callback());
+    }, [callback]);
+    return <div>
+        子组件{count}
+    </div>
+}
+
+// export default function App() {
+//     // const [state, dispatch] = useReducer(reducer, 1, init);
+//     const [count, setCount] = useState(1);
+//     const [val, setVal] = useState('')
+
+//     const callback = useCallback(() => {
+//         console.log(count);
+//     }, [count]);
+//     set.add(callback);
+//     return (
+//         <>
+//             <h4>count: {count}</h4>
+//             <h4>{set.size}</h4>
+//             {/* <button
+//                 onClick={() => dispatch({ type: 'reset', payload: 5 })}>
+//                 Reset
+//             </button> */}
+//             <button onClick={() => setCount(count + 1)}>+</button>
+//             <input value={val} onChange={event => setVal(event.target.value)}/>
+//         </>
+//     )
+// }
 
 
 // export default function App() {
