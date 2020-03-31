910ecf8e0ef7297f7788468e8097cf6b00cb64fa
cengchao <475383200@qq.com>
2020-03-31 14:21:11 +0800

    提交

--- react-learn/src/useScroll.js
+++ react-learn/src/useScroll.js
@@ -2,5 +2,6 @@ import { useEffect } from "react"
 import reset from "./resetScroll"
 
 export default function useScroll(pathname) {
+    console.log(pathname,'看看')
     useEffect(reset, [pathname])
 }
