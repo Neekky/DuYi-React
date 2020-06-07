06bba0f6baf5a6916e8a307e650a0b6d9f1784e9
cengchao <475383200@qq.com>
2020-06-07 14:47:32 +0800

    代码提交

--- react-learn/src/pages.js
+++ react-learn/src/pages.js
@@ -15,6 +15,23 @@ export function NavBar() {
 export function Home() {
     return <div className="page home">
         <h1>首页</h1>
+        <a href="alipay://">跳转</a>
+        <video
+            src="blob:https://www.bilibili.com/580ee34e-d310-4f0f-94f9-217212f30b69"
+            style={{ width: '300px', height: '200px' }}
+            width="400px"
+            height="400px"
+            preload="preload"
+            controls="controls" 
+            autoPlay="autoplay"
+            x-webkit-airplay="true" 
+            x5-video-player-fullscreen="true"
+            playsInline 
+            webkit-playsinline="true"
+            x5-video-player-type="h5-page"
+            poster="https://static.caibeike.com/i/af4a8d135786ffcc0ffda0ef49f0ab4d-osG00u-biAweghp1"
+        >
+        </video>
     </div>
 }
 
