21027d1e5028a1fa360ccf3ed9b3cc5c24860388
cengchao <475383200@qq.com>
2020-07-14 14:54:02 +0800

    提交开发代码

--- react-learn/src/index.js
+++ react-learn/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from "./App"
 // import "./react-router/browserHistory"
 // import "./react-router/hashHistory"
-import "./react-router/memoryHistory"
+// import "./react-router/memoryHistory"
+import "./my-react-router/browserHistory"
 
 ReactDOM.render(<App/>, document.getElementById('root'));
