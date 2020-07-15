417c0b6561b0d41df7076afed318a3ac8534621e
cengchao <475383200@qq.com>
2020-07-15 16:16:49 +0800

    提交代码

--- react-learn/src/myRedux/index.js
+++ react-learn/src/myRedux/index.js
@@ -5,7 +5,11 @@ import * as usersAction from "./action/usersAction";
 import uuid from 'uuid';
 
 const store = createStore(reducer);
- 
+
+const unListen = store.subscribe(() => {
+    console.log(store.getState(),'unListen');
+})
+
 store.dispatch(usersAction.createAddUserAction({id: uuid(), name: "韭菜", age: 22 }))
 store.dispatch(loginUserAction.createSetLoginUserAction({id: uuid(), name: "韭菜", age: 22 }))
 console.log(store.getState());
