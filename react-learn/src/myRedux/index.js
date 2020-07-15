0b702faa2849c746efa3e3334b51aa41e5b0c155
cengchao <475383200@qq.com>
2020-07-15 15:25:02 +0800

    提交代码

---
+++ react-learn/src/myRedux/index.js
@@ -0,0 +1,22 @@
+import { createStore, bindActionCreators } from "redux";
+import reducer from "./reducer";
+import * as loginUserAction from "./action/loginUserAction";
+import * as usersAction from "./action/usersAction";
+import uuid from 'uuid';
+
+const store = createStore(reducer);
+ 
+store.dispatch(usersAction.createAddUserAction({id: uuid(), name: "韭菜", age: 22 }))
+store.dispatch(loginUserAction.createSetLoginUserAction({id: uuid(), name: "韭菜", age: 22 }))
+console.log(store.getState());
+// function myCreateStore(reducer, store) {
+//     let myStore = store;
+//     return {
+//         dispatch: function (action) {
+//             myStore = reducer(myStore, action);
+//         },
+//         getState: function () {
+//             console.log( );
+//         }
+//     }
+// }
