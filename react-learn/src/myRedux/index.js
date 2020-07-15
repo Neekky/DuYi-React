e38d2c9b9114e632f186b12ab60781b435a014a4
cengchao <475383200@qq.com>
2020-07-15 20:15:08 +0800

    修改代码

--- react-learn/src/myRedux/index.js
+++ react-learn/src/myRedux/index.js
@@ -1,4 +1,5 @@
 import { createStore } from "./myCreateStore";
+import { bindActionCreators } from "./myBindActionCreators";
 import reducer from "./reducer";
 import * as loginUserAction from "./action/loginUserAction";
 import * as usersAction from "./action/usersAction";
@@ -6,15 +7,15 @@ import uuid from 'uuid';
 
 const store = createStore(reducer);
 
+const boundUserAction = bindActionCreators({...usersAction,...loginUserAction}, store.dispatch);
+
 const unListen1 = store.subscribe(() => {
-    console.log(store.getState(),'订阅者1');
+    console.log(store.getState(), '订阅者1');
 })
 
-const unListen2 = store.subscribe(() => {
-    console.log(store.getState(),'订阅者2');
-})
+boundUserAction.createAddUserAction({ id: uuid(), name: "韭菜", age: 22 })
+unListen1();
+boundUserAction.createSetLoginUserAction({ id: uuid(), name: "韭菜", age: 22 });
+boundUserAction.createSetLoginUserAction({ id: uuid(), name: "大庄", age: 25 });
 
-store.dispatch(usersAction.createAddUserAction({id: uuid(), name: "韭菜", age: 22 }))
-unListen1()
-store.dispatch(loginUserAction.createSetLoginUserAction({id: uuid(), name: "韭菜", age: 22 }))
 console.log(store.getState());
