34b2bbba85ce4f782ff6b523b95d73ff27d6ab35
cengchao <475383200@qq.com>
2020-07-23 16:57:36 +0800

    代码改动

--- react-learn/src/store/mySaga/studentTask.js
+++ react-learn/src/store/mySaga/studentTask.js
@@ -1,13 +1,15 @@
 import { actionTypes, setIsLoading, setStudentsAndTotal,  } from "../action/student/searchResult"
 import { takeEvery, put, cps, select } from "redux-saga/effects"
+import {searchStudents} from "../../services/student"
 
 function* fetchStudents() {
     // 设置为正在加载中
     yield put(setIsLoading(true));
+    const resp = yield searchStudents();
+    console.log(resp,'服务器响应结果')
 }
 
 export default function* () {
     yield takeEvery(actionTypes.fetchStudents, fetchStudents)
     console.log('正在监听 fetchStudents'); 
-    yield 
 }
