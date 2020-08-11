2044ebca33d2fba5d9a64f5b7645fc587d08e17d
cengchao <475383200@qq.com>
2020-08-11 17:29:18 +0800

    提交代码

--- umi-learn/pages/page1.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function page1() {
-    return (
-        <div>
-            <h1>页面1</h1>
-        </div>
-    )
-}
--- umi-learn/pages/page2.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function page2() {
-    return (
-        <div>
-            <h1>页面2</h1>
-        </div>
-    )
-}
